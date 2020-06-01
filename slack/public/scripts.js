39b51f6940dba280ec006350aa524e77e0730e47
Alexander De Michieli <ademichieli@squarespace.com>
2020-05-31 23:06:03 -0700

    saving

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,32 +1,78 @@
 const username = prompt("What is your username?")
-// const socket = io('http://localhost:9000'); // the / namespace/endpoint
-const socket = io('http://localhost:9000',{
-    query: {
-        username
-    }
-});
-let nsSocket = "";
-// listen for nsList, which is a list of all the namespaces.
-socket.on('nsList',(nsData)=>{
-    console.log("The list of .rooms has arrived!!")
-    // console.log(nsData)
-    let namespacesDiv = document.querySelector('.namespaces');
-    namespacesDiv.innerHTML = "";
-    nsData.forEach((ns)=>{
-        namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`
-    })
+const socket = io('http://localhost:9000'); 
+const socket2 = io('http://localhost:9000/wiki') //Namespace
+const socket3 = io('http://localhost:9000/mozilla') //Namespace
+const socket4 = io('http://localhost:9000/linux') //Namespace
 
-    // Add a clicklistener for each NS
-    console.log(document.getElementsByClassName('namespace'))
-    Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
-        // console.log(elem)
-        elem.addEventListener('click',(e)=>{
-            const nsEndpoint = elem.getAttribute('ns');
-            // console.log(`${nsEndpoint} I should go to now`)
-            joinNs(nsEndpoint)
-        })
-    })
-    joinNs('/wiki');
+
+//JS for Client
+const squarespace = io('http://localhost:9000/custops') //Namespace
+
+//Test message from server
+socket.on('msg_from_server_to_client', (message)=>{
+    console.log(message);
+    //In the same function, sending a test message to server
+    socket.emit('message_to_server_from_client', {data: 'message to server'});
+});
+
+//Function when pressing the Submit button
+document.querySelector('#message-form').addEventListener('submit', (event)=>{
+event.preventDefault();
+
+//Grabbing the message and sending it to the server
+const chatMessage = document.querySelector('#user-message').value
+socket.emit('newMessage_to_server_from_client', {data: chatMessage});
+
+});
+
+//Receiving back the message from the server
+socket.on('messageToClients', (data)=>{
+const message = data.text
+document.querySelector("#messages").innerHTML += `<li>${message}</li>`
 })
+
+///////////////////////////////
+///////Second Namespace/////////
+///////////////////////////////
+
+squarespace.on('connect', ()=>{
+    console.log("Squarespace", squarespace.id);    
+});
+
+squarespace.on('welcome_to_custops', (message)=>{
+    console.log(message);    
+});
+
+
+
+// const socket = io('http://localhost:9000',{
+//     query: {
+//         username
+//     }
+// });
+
+// let nsSocket = "";
+// // listen for nsList, which is a list of all the namespaces.
+// socket.on('nsList',(nsData)=>{
+//     console.log("The list of .rooms has arrived!!")
+//     // console.log(nsData)
+//     let namespacesDiv = document.querySelector('.namespaces');
+//     namespacesDiv.innerHTML = "";
+//     nsData.forEach((ns)=>{
+//         namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`
+//     })
+
+//     // Add a clicklistener for each NS
+//     console.log(document.getElementsByClassName('namespace'))
+//     Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
+//         // console.log(elem)
+//         elem.addEventListener('click',(e)=>{
+//             const nsEndpoint = elem.getAttribute('ns');
+//             // console.log(`${nsEndpoint} I should go to now`)
+//             joinNs(nsEndpoint)
+//         })
+//     })
+//     joinNs('/wiki');
+// })
 
 
--- slack/slack.js
+++ slack/slack.js
@@ -1,91 +1,46 @@
-const express = require('express');
-const app = express();
+//SERVER
+
+const express = require('express')
+const app = express()
 const socketio = require('socket.io')
+let namespaces = require('./data/namespaces')
 
-let namespaces = require('./data/namespaces');
-// console.log(namespaces[0]);
+const expressServer = app.listen(9000)
+const io = socketio(expressServer)
 app.use(express.static(__dirname + '/public'));
-const expressServer = app.listen(9000);
-const io = socketio(expressServer);
 
 
-// io.on = io.of('/').on = io.sockets.on
-// io.emit = io.of('/').emit = io.sockets.emit
-io.on('connection',(socket)=>{
-    // console.log(socket.handshake)
-    // build an array to send back with the img and endpoing for each NS
-    let nsData = namespaces.map((ns)=>{
-        return {
-            img: ns.img,
-            endpoint: ns.endpoint
-        }
-    })
-    // console.log(nsData)
-    // sned the nsData back to the client. We need to use socket, NOT io, because we want it to 
-    // go to just this client. 
-    socket.emit('nsList',nsData);
-})
+//loop through each namespace and user server to listen for a connection
 
-
-// loop through each namespace and listen for a connection
-namespaces.forEach((namespace)=>{
-    // console.log(namespace)
-    // const thisNs = io.of(namespace.endpoint)
-    io.of(namespace.endpoint).on('connection',(nsSocket)=>{
-        console.log(nsSocket.handshake)
-        const username = nsSocket.handshake.query.username;
-        // console.log(`${nsSocket.id} has join ${namespace.endpoint}`)
-        // a socket has connected to one of our chatgroup namespaces.
-        // send that ns gorup info back
-        nsSocket.emit('nsRoomLoad',namespace.rooms)
-        nsSocket.on('joinRoom',(roomToJoin,numberOfUsersCallback)=>{
-            // deal with history... once we have it
-            console.log(nsSocket.rooms);
-            const roomToLeave = Object.keys(nsSocket.rooms)[1];
-            nsSocket.leave(roomToLeave);
-            updateUsersInRoom(namespace, roomToLeave)
-            nsSocket.join(roomToJoin)
-            // io.of('/wiki').in(roomToJoin).clients((error, clients)=>{
-            //     console.log(clients.length)
-            //     numberOfUsersCallback(clients.length);
-            // })
-            const nsRoom = namespace.rooms.find((room)=>{
-                return room.roomTitle === roomToJoin;
-            })
-            nsSocket.emit('historyCatchUp', nsRoom.history)
-            updateUsersInRoom(namespace, roomToJoin);
-        })
-        nsSocket.on('newMessageToServer',(msg)=>{
-            const fullMsg = {
-                text: msg.text,
-                time: Date.now(),
-                username: username,
-                avatar: 'https://via.placeholder.com/30'
-            }
-            // console.log(fullMsg)
-            // Send this message to ALL the sockets that are in the room that THIS socket is in.
-            // how can we find out what rooms THIS socket is in?
-            // console.log(nsSocket.rooms)
-            // the user will be in the 2nd room in the object list
-            // this is because the socket ALWAYS joins its own room on connection
-            // get the keys
-            const roomTitle = Object.keys(nsSocket.rooms)[1];
-            // we need to find the Room object for this room
-            const nsRoom = namespace.rooms.find((room)=>{
-                return room.roomTitle === roomTitle;
-            })
-            // console.log("The room object that we made that matches this NS room is...")
-            // console.log(nsRoom)
-            nsRoom.addMessage(fullMsg);
-            io.of(namespace.endpoint).to(roomTitle).emit('messageToClients',fullMsg)
-        })
+namespaces.map(namespace => {
+    io.of(namespace.endpoint).on('connect', (socket)=>{
+        console.log(`${namespace.endpoint}`, socket.id)
     })
 })
 
-function updateUsersInRoom(namespace, roomToJoin){
-    // Send back the number of users in this room to ALL sockets connected to this room
-    io.of(namespace.endpoint).in(roomToJoin).clients((error,clients)=>{
-        // console.log(`There are ${clients.length} in this room`);
-        io.of(namespace.endpoint).in(roomToJoin).emit('updateMembers',clients.length)
+
+io.on('connect', (socket, req)=>{
+
+    //Test message coming from client
+    socket.emit('msg_from_server_to_client', 'welcome to websocket')
+
+    //Test message to client
+    socket.on('message_to_server_from_client',(message)=>{
+        console.log(message, socket.id)
     })
-}
+
+    socket.on('newMessage_to_server_from_client',(message)=>{
+        // console.log(message)
+        //io sends to all clients. If we did 'socket.emit' it would not send it to everyone
+        io.emit('messageToClients', {text: message.data})
+        //same as above, but for one namespace
+        // io.of('/').emit('messageToClients', {text: message.data})
+    })
+})
+
+//'Of' required when using other namespaces
+io.of('/custops').on('connect', (socket)=>{
+    console.log('connected to Squarespace custops')
+    io.of('/custops').emit('welcome_to_custops', "Welcome to custops channer")
+})
+
