a6d8c657834fae1c82ab6db0e723e4209e45e46d
Alexander De Michieli <ademichieli@squarespace.com>
2020-06-03 22:26:25 -0700

    save

--- slack/public/joinNs.js
+++ slack/public/joinNs.js
@@ -1,6 +1,9 @@
+//extension of scripts.js
+//client side
+
 function joinNs(endpoint){
 
-    const nsSocket = io(`http://localhost:9000${endpoint}`)
+    nsSocket = io(`http://localhost:9000${endpoint}`)
     nsSocket.on('nsRoomload', (nsRooms)=> {
         // console.log(nsRooms)
         let roomList = document.querySelector('.room-list')
@@ -22,7 +25,34 @@ function joinNs(endpoint){
             })
         //add room automatically. First time at login
         const topRoom = document.querySelector('.room').innerText
-        console.log(topRoom)
-
+        joinRoom(topRoom)
     })
+
+    //Function when pressing the Submit button
+    document.querySelector('.message-form').addEventListener('submit', (event)=>{
+        event.preventDefault();
+    //Grabbing the message and sending it to the server
+    const chatMessage = document.querySelector('#user-message').value
+        console.log(chatMessage)
+        nsSocket.emit('newMessage_to_server_from_client', {data: chatMessage});
+    });
+
+    nsSocket.on('message_to_clients_in_room', (message)=>{
+        document.querySelector("#messages").innerHTML +=  buildHTML(message)
+    })  
+    
+    function buildHTML(message){
+        const convertedDate = new Date(message.time).toLocaleString()
+        return  `
+        <li>
+            <div class="user-image">
+                <img src=${message.avatar} />
+            </div>
+            <div class="user-message">
+                <div class="user-name-time">${message.username}<span>${convertedDate}</span></div>
+                <div class="message-text">${message.text.data}</div>
+            </div>
+        </li>
+        `
+    }
 }
