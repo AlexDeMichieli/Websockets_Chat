f8138c7dfd057ee696136b78814420fca82e1832
Alexander De Michieli <ademichieli@squarespace.com>
2020-06-04 21:28:25 -0700

    updated history

--- slack/slack.js
+++ slack/slack.js
@@ -28,6 +28,7 @@ io.on('connect', (socket, req)=>{
 namespaces.forEach(namespace => {
     // console.log(namespace.endpoint)
     io.of(namespace.endpoint).on('connect', (nsSocket)=>{
+        
         console.log(`${nsSocket.id} has join ${namespace.endpoint}` )
         //a socket has connected to one of our namespaces, we need to send that to the group
         nsSocket.emit('nsRoomload', namespaces[0].rooms)
@@ -35,11 +36,20 @@ namespaces.forEach(namespace => {
             //roomToJoin == New Articles
             nsSocket.join(roomToJoin)
             //namespace.clients(callback)
-            io.of('/wiki').in(roomToJoin).clients((error,clients)=>{
-                console.log(clients.length)
-                //callback back to joinRoom
-                numberOfUsersCallback(clients.length)
+            // io.of('/wiki').in(roomToJoin).clients((error,clients)=>{
+            //     //callback back to joinRoom
+            //     numberOfUsersCallback(clients.length)
+            // })
+            //pushing history to the client so it can be printed in the chat when someone joins.
+            const nsRoom = namespaces[0].rooms.find((room=>{
+                return room.roomTitle === roomToJoin
+            }));
+            nsSocket.emit('history_catch_up', nsRoom.history)
+            //udating the number of users in all rooms
+            io.of('/wiki').in(roomToJoin).clients((error, clients)=>{
+                io.of('/wiki').in(roomToJoin).emit('update_members', clients.length)
             })
+
         })
         nsSocket.on('newMessage_to_server_from_client', (message)=>{
 
@@ -50,8 +60,16 @@ namespaces.forEach(namespace => {
                 username: 'superHero',
                 avatar: 'https://via.placeholder.com/30'
             }
+           
             //use socket.rooms to find in which rooms the socket is connected to
             const roomTitle = Object.keys(nsSocket.rooms)[1]
+            //we need to push the chat history to new connections joining the room. 
+            //getting the room object for the room.
+            const nsRoom = namespaces[0].rooms.find((room=>{
+                return (room.roomTitle === roomTitle)
+            }));
+            //pushing history with object method
+            nsRoom.addMessage(fullMsg)
             //if we did NsSocket it will show up to all other clients
             io.of('/wiki').to(roomTitle).emit('message_to_clients_in_room', fullMsg)
 
