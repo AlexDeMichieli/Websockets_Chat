00c38f53b8b7620ab6eeccf311e2e2ab8dd40552
Alexander De Michieli <ademichieli@squarespace.com>
2020-06-17 09:50:26 -0700

    changing port

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@
 
 const username = prompt("What is your username?")
 let avatar = `https://api.adorable.io/avatars/40/${username}`
-const socket = io('http://localhost:9000/chat.html'); 
+const socket = io('process.env.PORT || 5000'); 
 let nsSocket = ''
 
 //populating DOM with Room Namespaces icons. nsList is sent from Server.
